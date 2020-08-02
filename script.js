78e8543a9c921b5c78cf06e43b0c54e03f0723c0
NandanPranto <44819059+NandanPranto@users.noreply.github.com>
2020-08-03 00:53:56 +0600

    assignment finished but some features can still be added

--- script.js
+++ script.js
@@ -1,49 +1,92 @@
 
-
+//pin code generate 
 const pinGenerateBtn = document.getElementById("generateBtn");
 pinGenerateBtn.addEventListener("click",function(){
-
-    var randomNum=Math.round(1000 + Math.random() * 9000);
-    document.getElementById("generateBtnDisplay").value = randomNum;
     
+        var randomNum=Math.round(1000 + Math.random() * 9000);//to take the random number of four numbers
+        document.getElementById("generateBtnDisplay").value = randomNum;
+        
 });
 
+
+
+//number pad 
+function onClickNumberPad(val){
+
+    document.getElementById("numberBtnDisplay").value += val; //number key te click korle value j number e click kora hoiche seta bosbe + jotobar click kora hobe display value er sathe add hotei thakbe
+    onlyFourKey();
+   
+      
+  }
+  
+  function onClickNumberPadClear(){
+      document.getElementById("numberBtnDisplay").value = '';
+     
+      
+      
+  }
+  
+  function onClickNumberPadBackspace(){
+      var val = document.getElementById("numberBtnDisplay").value;
+      document.getElementById("numberBtnDisplay").value = val.substr(0, val.length - 1); //prottekbar vale er length theke 1 kore komabe
+  
+      
+  }
+  
+  function onlyFourKey(){
+      var val = document.getElementById("numberBtnDisplay").value;
+      document.getElementById("numberBtnDisplay").value = val.substr(0, 4);//char ta number nibe
+  }
+  
+  const submitBtn = document.getElementById("submit-btn");
+  submitBtn.addEventListener("click",function(){
+    if(generateBtnDisplay.value == '' && numberBtnDisplay.value == ''){
+       document.getElementById("notifyZero").style.display = "inline" ;
+    }else{
+        matchTheGeneratedPinAndClickedPin(generateBtnDisplay);
+       
+        tryleft()
+    }
+   
+
+    
+   
+  });
+
+
+//to match
 function matchTheGeneratedPinAndClickedPin(id){
     if(id.value == numberBtnDisplay.value){
-        const message = document.getElementById("notifySuccess");
-        message.style.display = "inline" ;
-    }else{
-        const message = document.getElementById("notifyFail");
-        message.style.display = "inline" ;
+    
+        // document.getElementById("notifySuccess").style.display = "inline" ;
+        toDisplaySuccess()
+    }
+    else{
+        
+    //   document.getElementById("notifyFail").style.display = "inline" ;
+    toDisplayFail()
+       
     }
 }
 
-
-
-function onClickNumberPad(val){
-
-  document.getElementById("numberBtnDisplay").value += val;
-  onlyFourKey();
-    
+function tryleft(){
+    var three = document.getElementById("action-left").innerHTML;
+    var threeC = parseFloat(three);
+    if((threeC - 1) != -1){
+        document.getElementById("action-left").innerHTML= (threeC - 1);
+    }
+   
 }
 
-function onClickNumberPadClear(){
-    document.getElementById("numberBtnDisplay").value = '';
+function toDisplaySuccess(){
+    document.getElementById("notifySuccess").style.display = "inline" ;
+}
+function toDisplayFail(){
+    document.getElementById("notifyFail").style.display = "inline" ;
 }
 
-function onClickNumberPadBackspace(){
-    var val = document.getElementById("numberBtnDisplay").value;
-    document.getElementById("numberBtnDisplay").value = val.substr(0, val.length - 1);
-}
+// function notToDisplay(){
+//     document.getElementById("notify").style.display = "none" ;
+// }
 
-function onlyFourKey(){
-    var val = document.getElementById("numberBtnDisplay").value;
-    document.getElementById("numberBtnDisplay").value = val.substr(0, 4);
-}
-
-const submitBtn = document.getElementById("submit-btn");
-submitBtn.addEventListener("click",function(){
-
-    matchTheGeneratedPinAndClickedPin(generateBtnDisplay);
-});
 
